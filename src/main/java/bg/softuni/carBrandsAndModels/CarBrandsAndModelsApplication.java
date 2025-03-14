174cdcd9d75179442f42597476d9b031d921dfab
dimit <dimitar_rradev@abv.bg>
2025-03-14 10:25:17 +0200

    first commit

---
+++ src/main/java/bg/softuni/carBrandsAndModels/CarBrandsAndModelsApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.serviceschedulercarbrandsandmodels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSchedulerCarBrandsAndModelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceSchedulerCarBrandsAndModelsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bg/softuni/serviceschedulercarbrandsandmodels/ServiceSchedulerCarBrandsAndModelsApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.serviceschedulercarbrandsandmodels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceSchedulerCarBrandsAndModelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
